53b350e28abd833899502cca82370dabcccc0511
addicola <blue_lifesky@hotmail.com>
2025-09-24 11:21:46 +0700

    feat: create http server

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package backend
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Starting server on :8080")
+	log.Fatal(srv.ListenAndServe())
+}
