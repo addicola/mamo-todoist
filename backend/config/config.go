dca77634f45b3f899cbce74fed52d9e09593291c
addicola <blue_lifesky@hotmail.com>
2025-10-02 13:42:39 +0700

    feat: create config module

---
+++ backend/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+type (
+	Config struct {
+		Server     *Server
+		Blobstream *Blobstream
+	}
+
+	Server struct {
+		Port string
+	}
+
+	Blobstream struct {
+		RPC        string
+		Namaespace string
+	}
+)
+
+var (
+	once           sync.Once
+	configInstance *Config
+)
+
+func GetConfig() *Config {
+	once.Do(func() {
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("./")
+		viper.AutomaticEnv()
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
+		}
+
+		if err := viper.Unmarshal(&configInstance); err != nil {
+			panic(err)
+		}
+	})
+
+	return configInstance
+}
